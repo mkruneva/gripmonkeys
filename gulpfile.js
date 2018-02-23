d2865f09ec5a0d8b286d87a88186f22dfbdbae8a
mkruneva <marta.kruneva@gmail.com>
2018-02-23 15:52:52 +0000

    gulp js setup

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,24 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 
-gulp.task('styles', function() {
-    gulp.src('./client/static/assets/style/main.less')
+gulp.task('css', function() {
+    gulp.src('client/static/assets/style/main.less')
         .pipe(less())
         .pipe(minifyCSS())
-        .pipe(gulp.dest('./build/css'))
-        .pipe(browserSync.reload({ stream: true }));
+        .pipe(gulp.dest('build/css'))
+        // .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('js', function() {
+    return gulp.src('client/static/assets/js/application/source/application.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'))
 });
 
 gulp.task('serve', function() {
@@ -24,4 +34,4 @@ gulp.task('serve', function() {
     gulp.watch('./**/*.html').on('change', browserSync.reload());
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['css', 'js']);
